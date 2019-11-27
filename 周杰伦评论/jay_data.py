b6843d48de1caff8b34874ea784bd5d50427165b
Qical <34668455+Qical@users.noreply.github.com>
2019-11-27 21:44:08 +0800

    Update jay_data.py

--- 周杰伦评论/jay_data.py
+++ 周杰伦评论/jay_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 import re
 from collections import Counter # 用于字典类型的计数
 
+
 # 生成echarts类库表的调用
 from pyecharts.globals import SymbolType
 from pyecharts.charts import Bar,WordCloud
@@ -44,8 +45,62 @@ word = word_dict_sort[:100]
 
 # 制作词云图
 wordcloud = WordCloud()
-# 添加注释 
+# 添加注释
 wordcloud.add("Jay",words,word_size_range=[20,100],shape='circle')
+wordcloud.render_notebook()
+
+# 进行词性分类
+def speech_cut(speech):
+    word_list =[]
+    for word in word_dict_sort:
+        words = pseg.cut(word[0])
+        for w,flag in words:
+            if flag == speech:
+                word_list.append(word)
+    return word_list
+
+# 动词 V
+verb_word = speech_cut('v')
+wordcloud = WordCloud()
+wordcloud.add("",verb_word[:100],word_size_range=[20,100],shape='circle')
+wordcloud.render_notebook()
+
+# 名词 n
+verb_word = speech_cut('n')
+wordcloud = WordCloud()
+wordcloud.add("",verb_word[:100],word_size_range=[20,100],shape='circle')
+wordcloud.render_notebook()
 
 
+# 表情分析,爬下来的文本会有emoji，我们需要将其过滤掉
+"""
+具体的mysql的处理方式可以通过如下的链接查看
+https://blog.csdn.net/weixin_42812527/article/details/81876713
+有时需要过滤掉四字节以上的字符（表情），比如MySQL数据库5.5.3以下的版本text字段不支持四字节以上字符
+"""
 
+def get_emoji(content):
+    pattern = re.compile(u"[\U00010000-\U0010ffff]")
+    result = re.findall(pattern,content)
+    return result
+
+# apply的传参方法需要注意一下：
+df['emojis_list'] = df['comment'].apply(get_emoji)
+# 转变成列表(没有emoji就是空列表，有emoji就是emoji表情
+# eg:[[], [], ['😭'], ['😘'], [], [], [], ['😍'], ['😭'], ['🌹', '🌹', '🌹', '🌹']]
+emojis = df['emojis_list'].values.tolist()
+
+# 这个地方妙用了sum的方法，给例表实现了降维，并且去掉了空列表[],本应该用join的方式来实现
+# https://blog.csdn.net/BF02jgtRS00XKtCx/article/details/89484064可以大致参考这个博客进行查看
+emojis_list = sum(emojis,[])
+
+# 对列表的元素进行计数，并且生成字典
+# eg: {'😭': 261, '😍': 83, '😃': 79, '😂': 69, '👍': 67}
+counter = Counter(emojis_list)
+# most_common([n]):http://www.pythoner.com/205.html
+# 返回一个TopN列表。如果n没有被指定，则返回所有元素。当多个元素计数值相同时，排列是无确定顺序的。
+y_emojis, x_counts = zip(*counter.most_common())
+bar = Bar()
+bar.add_xaxis(y_emojis[:20])
+bar.add_yaxis("", x_counts[:20])
+bar.render_notebook()
